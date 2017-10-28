7a12e9d77752374299dcc865eb7975847234b6f0
F-9 <33187791+F-9@users.noreply.github.com>
2017-10-28 16:27:36 -0700

    Update and rename .gitignore to login.js

---
+++ .gitignore/login.js
@@ -0,0 +1,16 @@
+var http = new XMLHttpRequest();
+var url = "accounts/login/ajax/";
+var params = "password=asd123456&username=9__u";
+http.open("POST", url, true);
+
+//Send the proper header information along with the request
+http.setRequestHeader("Content-type", "application/json");
+http.setRequestHeader("x-csrftoken", "usEBdlnhAIsEU6Zj3I0IwqI7HMgvzNlB");
+http.setRequestHeader("x-instagram-ajax", "1");
+http.setRequestHeader("x-requested-with", "XMLHttpRequest");
+http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+}
+http.send(params);
